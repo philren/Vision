2757dbd810c35ff17f2bd848984a0be276bcdfa3
petershi <shixx@hyron-js.com>
2013-02-26 10:17:29 +0800

    add base MainActivity

--- GP Mobile/Android/GPVision/src/com/gpvision/activity/LoginActivity.java
+++ GP Mobile/Android/GPVision/src/com/gpvision/activity/LoginActivity.java
@@ -3,6 +3,7 @@ package com.gpvision.activity;
 import com.gpvision.R;
 import com.gpvision.utils.AppUtils;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -57,5 +58,8 @@ public class LoginActivity extends BaseActivity {
 		}
 
 		// TODO call API
+		Intent intent = new Intent();
+		intent.setClass(LoginActivity.this, MainActivity.class);
+		startActivity(intent);
 	}
 }
---
+++ GP Mobile/Android/GPVision/src/com/gpvision/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.gpvision.activity;
+
+import com.gpvision.R;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+
+}
