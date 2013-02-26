3be7f41c416e61c55c0628de89f31096ca225b75
petershi <shixx@hyron-js.com>
2013-02-26 17:15:58 +0800

    add video info fragment

---
+++ GP Mobile/Android/GPVision/src/com/gpvision/datamodel/Video.java
@@ -0,0 +1,36 @@
+package com.gpvision.datamodel;
+
+
+public class Video {
+    private long id;
+    private String name;
+    private Status status;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public enum Status {
+        Uploading, Uploaded, Indexing, Indexed, Fail, Deleted
+    }
+}
