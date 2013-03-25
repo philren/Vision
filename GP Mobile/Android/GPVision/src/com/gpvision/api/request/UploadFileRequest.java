f832b79bcef7c0cca92de389dee3c0bea8e9b3e7
petershi <shixx@hyron-js.com>
2013-03-25 17:46:48 +0800

    debug upload file

--- GP Mobile/Android/GPVision/src/com/gpvision/api/request/UploadFileRequest.java
+++ GP Mobile/Android/GPVision/src/com/gpvision/api/request/UploadFileRequest.java
@@ -2,9 +2,9 @@ package com.gpvision.api.request;
 
 import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.RandomAccessFile;
 import java.net.URL;
 import java.util.ArrayList;
 
@@ -15,18 +15,24 @@ import android.os.AsyncTask;
 import com.gpvision.api.APIError;
 import com.gpvision.api.APIResponse;
 import com.gpvision.api.APIResponseHandler;
+import com.gpvision.utils.AppUtils;
 import com.gpvision.utils.LocalDataBuffer;
 import com.gpvision.utils.LogUtil;
 
 public class UploadFileRequest<RESPONSE extends APIResponse> extends
 		AsyncTask<Void, Void, Integer> {
 
+	private static final int CHUNKED_SIZE = 1024 * 1024;
+	private static final String BOUNDARY = java.util.UUID.randomUUID()
+			.toString();
+	private static final String PREFIX = "--", LINEND = "\r\n";
+	private static final String MULTIPART_FROM_DATA = "multipart/form-data";
 	private APIResponseHandler<RESPONSE> responseHandler;
 
 	private String mUrl;
 	private ArrayList<String[]> mFiles;
 	private UploadedProgressCallback callback;
-	private boolean running = true;
+	private volatile boolean running = true;
 
 	public void setCallback(UploadedProgressCallback callback) {
 		this.callback = callback;
@@ -70,16 +76,29 @@ public class UploadFileRequest<RESPONSE extends APIResponse> extends
 	@Override
 	protected Integer doInBackground(Void... params) {
 
-		String BOUNDARY = java.util.UUID.randomUUID().toString();
-		String PREFIX = "--", LINEND = "\r\n";
-		String MULTIPART_FROM_DATA = "multipart/form-data";
-
 		int res = 0;
 
+		if (mFiles != null && mFiles.size() > 0)
+			for (int i = 0, num = mFiles.size(); i < num; i++) {
+				String[] param = mFiles.get(i);
+				String fileName = param[0];
+				String fileType = param[1];
+				String filePath = param[2];
+				File file = new File(filePath);
+				int chunkedSize = (int) (file.length() / CHUNKED_SIZE + 1);
+				for (int n = 0; n < chunkedSize; n++) {
+					long offset = n * CHUNKED_SIZE;
+					res = upload(fileName, fileType, file, offset);
+				}
+			}
+		return res;
+	}
+
+	private int upload(String fileName, String fileType, File file, long offset) {
+		int res = 0;
 		try {
 			URL uri = new URL(mUrl);
 			HttpsURLConnection conn = (HttpsURLConnection) uri.openConnection();
-			conn.setChunkedStreamingMode(0);
 
 			conn.setReadTimeout(5 * 1000);
 			conn.setDoInput(true);
@@ -88,13 +107,22 @@ public class UploadFileRequest<RESPONSE extends APIResponse> extends
 			conn.setRequestMethod("POST");
 			conn.setRequestProperty("connection", "keep-alive");
 			conn.setRequestProperty("Charsert", "UTF-8");
-			// conn.setRequestProperty("Transfer-Encoding", "chunked");
 
 			conn.setRequestProperty("Content-Type", MULTIPART_FROM_DATA
 					+ ";boundary=" + BOUNDARY);
 
 			conn.setRequestProperty("accept",
 					"application/json, text/javascript, */*; q=0.01");
+
+			int to = (int) (offset + CHUNKED_SIZE - 1);
+			if (to > file.length())
+				to = (int) file.length();
+			conn.setRequestProperty("content-range", "bytes " + offset + "-"
+					+ to + "/" + file.length());
+			LogUtil.logE("bytes " + offset + "-" + to + "/" + file.length());
+//			conn.setRequestProperty("uuid",
+//					AppUtils.getMd5(file, offset, CHUNKED_SIZE));
+//			LogUtil.logE(AppUtils.getMd5(file, offset, CHUNKED_SIZE));
 
 			if (LocalDataBuffer.getInstance().getAccount() != null) {
 				conn.setRequestProperty("endUserToken", LocalDataBuffer
@@ -103,39 +131,38 @@ public class UploadFileRequest<RESPONSE extends APIResponse> extends
 			DataOutputStream outStream = new DataOutputStream(
 					conn.getOutputStream());
 
-			if (mFiles != null && mFiles.size() > 0)
-				for (int i = 0, num = mFiles.size(); i < num; i++) {
-					String[] param = mFiles.get(i);
-					String fileName = param[0];
-					String fileType = param[1];
-					String filePath = param[2];
+			StringBuilder sb1 = new StringBuilder();
+			sb1.append(PREFIX);
+			sb1.append(BOUNDARY);
+			sb1.append(LINEND);
+			sb1.append("Content-Disposition: form-data; name=\"file[]\"; filename=\""
+					+ fileName + "\"" + LINEND);
+			sb1.append("Content-Type: " + fileType + LINEND);
+			sb1.append("uuid:" + AppUtils.getMd5(file, offset, CHUNKED_SIZE)
+					+ LINEND);
+			sb1.append(LINEND);
+			outStream.write(sb1.toString().getBytes());
 
-					StringBuilder sb1 = new StringBuilder();
-					sb1.append(PREFIX);
-					sb1.append(BOUNDARY);
-					sb1.append(LINEND);
-					sb1.append("Content-Disposition: form-data; name=\"file[]\"; filename=\""
-							+ fileName + "\"" + LINEND);
-					sb1.append("Content-Type: " + fileType + LINEND);
-					sb1.append(LINEND);
-					outStream.write(sb1.toString().getBytes());
-
-					File file = new File(filePath);
-					InputStream is = new FileInputStream(file);
-					byte[] buffer = new byte[1024];
-					int len = 0;
-					int uploadedSize = 0;
-					while (running && (len = is.read(buffer)) != -1) {
-						outStream.write(buffer, 0, len);
-						uploadedSize += len;
-						if (callback != null) {
-							callback.uploadedProgress(uploadedSize);
-						}
-					}
-
-					is.close();
-					outStream.write(LINEND.getBytes());
+			RandomAccessFile is = new RandomAccessFile(file, "r");
+			is.seek(offset);
+			byte[] buffer = new byte[1024];
+			int len = 0;
+			int count = 0;
+			while (running && (len = is.read(buffer)) != -1) {
+				count += len;
+				if (count < CHUNKED_SIZE) {
+					outStream.write(buffer, 0, len);
+				} else {
+					outStream.write(buffer, 0, 1024 - (count - CHUNKED_SIZE));
+					break;
 				}
+				if (callback != null) {
+					callback.uploadedProgress(count);
+				}
+			}
+
+			is.close();
+			outStream.write(LINEND.getBytes());
 
 			byte[] end_data = (PREFIX + BOUNDARY + PREFIX + LINEND).getBytes();
 			outStream.write(end_data);
@@ -159,7 +186,6 @@ public class UploadFileRequest<RESPONSE extends APIResponse> extends
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 		return res;
 	}
 
--- GP Mobile/Android/GPVision/src/com/gpvision/utils/AppUtils.java
+++ GP Mobile/Android/GPVision/src/com/gpvision/utils/AppUtils.java
@@ -1,9 +1,19 @@
 package com.gpvision.utils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import android.content.Context;
 import android.widget.Toast;
 
 public class AppUtils {
+	private static char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7',
+			'8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+
 	public static boolean isEmpty(String string) {
 		return string == null || string.length() == 0;
 	}
@@ -26,5 +36,59 @@ public class AppUtils {
 
 	public static String precentFormat(float x) {
 		return String.format("%.02f", x) + "%";
+	}
+
+	public static String getMd5(File file, long offset, int length) {
+		RandomAccessFile accessFile = null;
+		String md5 = null;
+		try {
+			accessFile = new RandomAccessFile(file, "r");
+			accessFile.seek(offset);
+			MessageDigest digest = MessageDigest.getInstance("MD5");
+			byte[] bytes = new byte[1024];
+			int count = 0;
+			int len = 0;
+			while ((len = accessFile.read(bytes)) != -1) {
+				count = +len;
+				if (count < length) {
+					digest.update(bytes, 0, len);
+				} else {
+					digest.update(bytes, 0, 1024 - (count - length));
+					break;
+				}
+			}
+			byte[] b = digest.digest();
+			md5 = bufferToHex(b, 0, b.length);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} finally {
+			if (accessFile != null)
+				try {
+					accessFile.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
+		return md5;
+	}
+
+	private static String bufferToHex(byte bytes[], int m, int n) {
+		StringBuffer stringbuffer = new StringBuffer(2 * n);
+		int k = m + n;
+		for (int l = m; l < k; l++) {
+			appendHexPair(bytes[l], stringbuffer);
+		}
+		return stringbuffer.toString();
+	}
+
+	private static void appendHexPair(byte bt, StringBuffer stringbuffer) {
+		char c0 = hexDigits[(bt & 0xf0) >> 4];
+		char c1 = hexDigits[bt & 0xf];
+		stringbuffer.append(c0);
+		stringbuffer.append(c1);
 	}
 }
