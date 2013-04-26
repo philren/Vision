9a2e78c0dcceba993372053dcaf50e8143b7284d
petershi <shixx@hyron-js.com>
2013-04-26 10:03:09 +0800

    update server

--- GP Mobile/Android/GPVision/src/com/gpvision/utils/Environment.java
+++ GP Mobile/Android/GPVision/src/com/gpvision/utils/Environment.java
@@ -2,10 +2,8 @@ package com.gpvision.utils;
 
 public class Environment {
 
-	public static final Environment E9 = new Environment("172.20.230.9:3800",
-			null);
-	public static final Environment E9S = new Environment("172.20.230.9:3801",
-			null);
+	public static final Environment E9 = new Environment("121.199.1.247", null);
+	public static final Environment E9S = new Environment("121.199.1.247", null);
 	private final String host;
 	private final String basePath;
 
